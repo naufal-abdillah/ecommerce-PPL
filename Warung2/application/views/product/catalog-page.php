fce9c6fc748a5ebdf8330621add971e82bf31d98
Aldi-Tri <aldi.t.m@mail.ugm.ac.id>
2020-11-21 11:41:43 +0700

    delete unused filters

--- Warung2/application/views/product/catalog-page.php
+++ Warung2/application/views/product/catalog-page.php
@@ -62,17 +62,6 @@
                                             <div class="form-check"><input class="form-check-input" type="checkbox" id="formCheck-3"><label class="form-check-label" for="formCheck-3">PC</label></div>
                                             <div class="form-check"><input class="form-check-input" type="checkbox" id="formCheck-4"><label class="form-check-label" for="formCheck-4">Tablets</label></div>
                                         </div>
-                                        <div class="filter-item">
-                                            <h3>Brands</h3>
-                                            <div class="form-check"><input class="form-check-input" type="checkbox" id="formCheck-5"><label class="form-check-label" for="formCheck-5">Samsung</label></div>
-                                            <div class="form-check"><input class="form-check-input" type="checkbox" id="formCheck-6"><label class="form-check-label" for="formCheck-6">Apple</label></div>
-                                            <div class="form-check"><input class="form-check-input" type="checkbox" id="formCheck-7"><label class="form-check-label" for="formCheck-7">HTC</label></div>
-                                        </div>
-                                        <div class="filter-item">
-                                            <h3>OS</h3>
-                                            <div class="form-check"><input class="form-check-input" type="checkbox" id="formCheck-8"><label class="form-check-label" for="formCheck-8">Android</label></div>
-                                            <div class="form-check"><input class="form-check-input" type="checkbox" id="formCheck-9"><label class="form-check-label" for="formCheck-9">iOS</label></div>
-                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -91,7 +80,7 @@
                                                         <div class="about">
                                                             <!-- <div class="rating"><img src="assets/img/star.svg"><img src="assets/img/star.svg"><img src="assets/img/star.svg"><img src="assets/img/star-half-empty.svg"><img src="assets/img/star-empty.svg"></div> -->
                                                             <div class="price">
-                                                                <h3>RP. <?php echo number_format($brg->harga,0,',','.') ?></h3>
+                                                                <h3>RP. <?php echo number_format($brg->harga, 0, ',', '.') ?></h3>
                                                             </div>
                                                             <!--Shopping cart-->
                                                             <?php echo anchor('cart/add_cart/' . $brg->id_barang, '<div class="btn btn-sm btn-success">Add to Cart</div>') ?>
